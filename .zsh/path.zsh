dc1a06129fa84c8493bdb8df45b42ea1823ff68f
Benoit Bar <benoit.bar@gandi.net>
2016-10-04 13:51:02 +0200

    Rename dofiler.zsh in path.zsh and add paths

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -1,13 +1,15 @@
 # Add dotfiler's bin directory to the path
-
 if [ -e ~/.dotfiles/bin ]; then
    export PATH=~/.dotfiles/bin:$PATH
 fi
 
-
-# And use ~/.bin directory to link
+# Use ~/.bin directory to link
 # all useful scripts into it
-
 if [ -e ~/.bin ]; then
    export PATH=~/.bin:$PATH
+fi
+
+# Add .local/bin directory to the path
+if [ -e ~/.local/bin ]; then
+   export PATH=~/.local/bin:$PATH
 fi
